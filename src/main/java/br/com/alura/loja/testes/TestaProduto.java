b6a9c27366354a164652b523ce34e3a4b8200310
Matheus Rhazek <50714597+Rhazek@users.noreply.github.com>
2024-06-24 11:53:52 -0300

    Add files via upload

--- src/main/java/br/com/alura/loja/testes/TestaProduto.java
+++ src/main/java/br/com/alura/loja/testes/TestaProduto.java
@@ -8,21 +8,40 @@ import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class TestaProduto {
     public static void main(String[] args) {
+        cadastrarProduto();
+        Long id = 1l;
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
 
-        Produto celular = new Produto("iPhone 13", "Modinha", new BigDecimal("3700"), Categoria.CELULARES);
+        Produto p = produtoDao.buscarProduto(1l);
+        System.out.println(p.getPreco());
+
+        List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+        todos.forEach(p3 -> System.out.println(p3.getNome()));
+
+        BigDecimal p2 = produtoDao.buscarPrecoDoProdutoPorNome("Galaxy S23");
+        System.out.println(p2);
+    }
+
+    private static void cadastrarProduto() {
+//        Categoria celurares = new Categoria("CELULARES");
+//        Produto celular = new Produto("iPhone 15 Pro Max", "O mais caro de todos", new BigDecimal("6700"), celurares);
 
         EntityManager em = JPAUtil.getEntityManager();
 
-        CategoriaDao categoriaDao = new CategoriaDao(em)
+        CategoriaDao categoriaDao = new CategoriaDao(em);
         ProdutoDao produtoDao = new ProdutoDao(em);
 
         em.getTransaction().begin();
-        produtoDao.cadastrar(celular);
+//
+//        categoriaDao.cadastrar(celurares);
+//        produtoDao.cadastrar(celular);
+
         em.getTransaction().commit();
         em.close();
-
     }
 }
